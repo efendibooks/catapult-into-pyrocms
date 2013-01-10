bcdfa814a0a616fe7409374f6b2e0aa9f3ee3f97
Jamie Rumbelow <jamie@jamierumbelow.net>
2013-01-10 19:42:34 +0000

    The beginnings of a theme

---
+++ addons/default/themes/catapult/theme.php
@@ -0,0 +1,11 @@
+<?php
+
+class Theme_Catapult extends Theme
+{
+	public $name = 'Catapult into PyroCMS';
+	public $author = 'Phil Sturgeon';
+	public $author_website = 'http://philsturgeon.co.uk';
+	public $website = 'https://efendibooks.com/books/catapult-into-pyrocms';
+	public $description = "The Catapult Into PyroCMS mini-site theme";
+	public $version = '1.0';
+}
