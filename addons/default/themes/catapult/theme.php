4ae5a4a92e8df55c1d4a2120b938b42c1e24c383
Jamie Rumbelow <jamie@jamierumbelow.net>
2013-02-02 15:14:34 +0000

    Adding a tonne of updates to the theme

--- addons/default/themes/catapult/theme.php
+++ addons/default/themes/catapult/theme.php
@@ -2,7 +2,7 @@
 
 class Theme_Catapult extends Theme
 {
-	public $name = 'Catapult into PyroCMS';
+	public $name = 'All About PyroCMS';
 	public $author = 'Phil Sturgeon';
 	public $author_website = 'http://philsturgeon.co.uk';
 	public $website = 'https://efendibooks.com/books/catapult-into-pyrocms';
